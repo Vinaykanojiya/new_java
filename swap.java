50dba7f42935d68de50a9fb11cd69abbe047188a
vinaykanojiya <kanojiyavinay112@gmail.com>
2023-11-21 10:34:30 +0530

    swap-second commit

--- swap.java
+++ swap.java
@@ -2,14 +2,11 @@ public class swap {
     public static void main(String[] args) {
     int n1 = 20;
     int n2 = 10;
-    // int vada=0;
-    // vada = n1;
-    // n1 = n2;
-    // n2 = vada;
-
-    n1 = n1 + n2;
-    n2 = n1 - n2;
-    n1 = n1 - n2;
+    int vada=0;
+    
+    vada = n1;
+    n1 = n2;
+    n2 = vada;
     
     System.out.println(n1);
     System.out.println(n2);
